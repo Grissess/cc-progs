392d8791ddab21abb2754f2b0d77d3376f55dd3d
Grissess <grissess@nexusg.org>
2016-12-13 22:50:10 -0500

    Rev 30: Support for OC

---
+++ manifest.cc
@@ -0,0 +1,5 @@
+programs/vim programs/vim
+programs/bootstrap programs/bootstrap
+programs/wget programs/wget
+apis/libtedit apis/libtedit
+.manifest manifest.cc
--- mkmanifest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# In English: ignore .git directories, then select files that aren't *.sh and
-# print them without the leading "start point" (of "./"). Yes, this condition
-# includes .manifest itself, this is intended.
-find . -name ".git" -prune -o \! -name "*.sh" -type f -fprintf .manifest "%P\n"
