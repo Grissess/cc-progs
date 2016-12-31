accb0de465598dc11be1ed391945391f6371ac8c
Grissess <grissess@nexusg.org>
2016-12-31 11:11:49 -0500

    Initial daemon support

--- manifest.cc
+++ manifest.cc
@@ -1,7 +1,7 @@
-!Manifest revision 1
-!Library versions 38
-programs/vim programs/vim
+!Manifest revision 2
 programs/bootstrap programs/bootstrap
+programs/vim programs/vim
 programs/wget programs/wget
 apis/libtedit apis/libtedit
+daemon/heartbeat daemons/heartbeat
 .manifest manifest.cc
