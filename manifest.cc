d9fd9153bca30faf3bdbf845aa7719e0fede7bb7
Grissess <grissess@nexusg.org>
2016-12-31 20:13:48 -0500

    added 2d acceleration

--- manifest.cc
+++ manifest.cc
@@ -5,5 +5,6 @@ programs/dctl programs/dctl
 programs/wget programs/wget
 apis/libtedit apis/libtedit
 apis/libdaemon apis/libdaemon
+apis/lib2daccel apis/lib2daccel
 opt/daemon/heartbeat daemons/heartbeat
 .manifest manifest.cc
