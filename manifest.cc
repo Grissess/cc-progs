949646a69d9867972b5e02d7946bf598778326c2
Graham Northup <grissess@nexusg.org>
2017-11-22 10:31:00 -0500

    Updated manifests to comply with COPYING :)

--- manifest.cc
+++ manifest.cc
@@ -1,4 +1,4 @@
-!Manifest revision 2
+!Manifest revision 3
 programs/bootstrap programs/bootstrap
 programs/vim programs/vim
 programs/dctl programs/dctl
@@ -8,3 +8,4 @@ apis/libdaemon apis/libdaemon
 apis/lib2daccel apis/lib2daccel
 opt/daemon/heartbeat daemons/heartbeat
 .manifest manifest.cc
+COPYING COPYING
