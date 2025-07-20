acfed96f7132df43f9330e35354d0544b6b3de56
Grissess <grissess@nexusg.org>
2025-07-19 23:04:19 -0400

    Respond to cc-tweaked/CC-Tweaked#739

--- manifest.cc
+++ manifest.cc
@@ -1,15 +1,15 @@
-!Manifest revision 6
+!Manifest revision 7
 programs/bootstrap programs/bootstrap
 programs/vim programs/vim
 programs/dctl programs/dctl
-apis/libtedit apis/libtedit
-apis/libdaemon apis/libdaemon
-apis/lib2daccel apis/lib2daccel
-apis/libccevent apis/libccevent
-apis/libccpackage apis/libccpackage
-apis/libacid apis/libacid
-apis/libbit apis/libbit
-apis/libgetopt apis/libgetopt
+programs/lib/libtedit apis/libtedit
+programs/lib/libdaemon apis/libdaemon
+programs/lib/lib2daccel apis/lib2daccel
+programs/lib/libccevent apis/libccevent
+programs/lib/libccpackage apis/libccpackage
+programs/lib/libacid apis/libacid
+programs/lib/libbit apis/libbit
+programs/lib/libgetopt apis/libgetopt
 opt/daemon/heartbeat daemons/heartbeat
 .manifest manifest.cc
 COPYING COPYING
